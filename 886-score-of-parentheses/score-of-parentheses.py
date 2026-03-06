8190dcc3345084b61d083294239daf27ff8bf1d3
Sriprakash <146555041+sriprakash001@users.noreply.github.com>
2026-03-06 22:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (81.40%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack = []
+        score = 0
+        for i in s:
+            if i== '(':
+                stack.append(score)
+                score = 0
+            else:
+                pre = stack.pop()
+                score = pre + max(2*score,1)
+
+        return score
