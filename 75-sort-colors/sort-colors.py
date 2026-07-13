711cba6475465581ca540062f60b8c172ee6b6cf
Sriprakash <146555041+sriprakash001@users.noreply.github.com>
2026-07-13 19:11:07 +0530

    Time: 3 ms (10.59%) | Memory: 19.4 MB (21.40%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -7,14 +7,52 @@ class Solution:
         #             nums[i+1],nums[i] = nums[i],nums[i+1]
 
         # Selection sort
-        pos = 0
-        while pos < len(nums)-1:
-            mini = pos
-            for i in range(pos+1,len(nums)):
-                if nums[i] < nums[mini]:
-                    mini = i
-            nums[pos],nums[mini] = nums[mini],nums[pos]
-            pos += 1
+        # pos = 0
+        # while pos < len(nums)-1:
+        #     mini = pos
+        #     for i in range(pos+1,len(nums)):
+        #         if nums[i] < nums[mini]:
+        #             mini = i
+        #     nums[pos],nums[mini] = nums[mini],nums[pos]
+        #     pos += 1
+        
+        # Merge sort:
+        
+        # split the nums
+        if len(nums) > 1:
+            mid = len(nums)//2
+            left = nums[:mid]
+            right = nums[mid:]
+
+            self.sortColors(left)
+            self.sortColors(right)
+
+            # Pointers
+            lp = 0
+            rp = 0
+            fp = 0
+
+            while lp < len(left) and rp < len(right):
+                if left[lp] < right[rp]:
+                    nums[fp] = left[lp]
+                    lp += 1
+                else:
+                    nums[fp] = right[rp]
+                    rp += 1
+                fp += 1
+            
+            while lp < len(left):
+                nums[fp] = left[lp]
+                lp += 1
+                fp += 1
+            
+            while rp < len(right):
+                nums[fp] = right[rp]
+                rp += 1
+                fp += 1
+
+
+
                     
 
 
