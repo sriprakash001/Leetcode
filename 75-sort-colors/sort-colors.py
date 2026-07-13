5b0e035e8b7599c89cfe801f7f0e543dc296e8e1
Sriprakash <146555041+sriprakash001@users.noreply.github.com>
2026-07-13 18:08:48 +0530

    Time: 3 ms (10.59%) | Memory: 19.2 MB (90.01%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,9 +1,22 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        for j in range(len(nums)-1):
-            for i in range(len(nums)-j-1):
-                if nums[i] > nums[i+1]:
-                    nums[i+1],nums[i] = nums[i],nums[i+1]
+        # Bubble sort
+        # for j in range(len(nums)-1):
+        #     for i in range(len(nums)-j-1):
+        #         if nums[i] > nums[i+1]:
+        #             nums[i+1],nums[i] = nums[i],nums[i+1]
+
+        # Selection sort
+        pos = 0
+        while pos < len(nums)-1:
+            mini = pos
+            for i in range(pos+1,len(nums)):
+                if nums[i] < nums[mini]:
+                    mini = i
+            nums[pos],nums[mini] = nums[mini],nums[pos]
+            pos += 1
+                    
+
 
 
         
