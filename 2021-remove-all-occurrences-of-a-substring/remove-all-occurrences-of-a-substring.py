9bf43982879a7cf9c1d133f13ff9ac71d2072b8a
Sriprakash <146555041+sriprakash001@users.noreply.github.com>
2026-08-01 15:20:16 +0530

    Time: 15 ms (27.01%) | Memory: 19.2 MB (91.48%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -4,10 +4,11 @@ class Solution:
         m = len(part)
         for ch in s:
             stack.append(ch)
-            if len(stack) >= m and ''.join(stack[-m:])== part:
+            if len(stack) >= m and ''.join(stack[-m:])== part: # to check last char equal to part or not 
                 for i in range(m):
                     stack.pop()
         return "".join(stack)
+        
         # Another approch
         # while part in s:
         #     s = s.replace(part,"",1)
