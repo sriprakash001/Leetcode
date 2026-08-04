3559f59a83e843512d5b9054d1d98b65563710d4
Sriprakash <146555041+sriprakash001@users.noreply.github.com>
2026-08-04 23:12:36 +0530

    Time: 2 ms (75.11%) | Memory: 19.4 MB (18.85%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -3,6 +3,6 @@ class Solution:
         nums1 = set(nums)
         ans = []
         for i in range(min(nums),max(nums)+1):
-            if i not in nums:
+            if i not in nums1:
                 ans.append(i)
         return ans
