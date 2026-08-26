28071bf04c3dace7301b12fbe7eec9b81bfae837
Sriprakash <146555041+sriprakash001@users.noreply.github.com>
2026-08-26 19:48:37 +0530

    Time: 418 ms (22.22%) | Memory: 19.5 MB (20.63%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -7,7 +7,7 @@ class Solution:
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             for j in range(i+1,n):
-                if j > i+1 and nums[j] == nums[j-1]:
+                if j>i+1 and nums[j] == nums[j-1]:
                     continue
                 left = j+1
                 right = n-1
@@ -17,8 +17,10 @@ class Solution:
                         ans.append([nums[i] , nums[j] , nums[left] , nums[right]])
                         left += 1
                         right -= 1
-                        while left < right and nums[left] == nums[left-1]:
-                            left += 1
+                        # Remove the duplicates of left 
+                        while left <  right and nums[left] == nums[left-1]:
+                            left  += 1
+                        # Remove the duplicates of right
                         while left < right and nums[right] == nums[right+1]:
                             right -= 1
                     elif summ < target:
